16f83203d820ee175b1d03fe76b48029d9216515
raushankumar <raushankumar2788@gmail.com>
2025-11-07 13:04:03 +0530

    commit1

---
+++ src/main/java/com/kumar/reactive/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.kumar.reactive.repository;
+
+import com.kumar.reactive.dto.ProductDto;
+import com.kumar.reactive.entity.Product;
+import org.springframework.data.domain.Range;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface ProductRepository extends ReactiveMongoRepository<Product,String> {
+    Flux<ProductDto> findByPriceBetween(Range<Double> priceRange);
+}
